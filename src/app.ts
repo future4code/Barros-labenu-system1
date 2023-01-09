c0009ac8cad30202daccef21e64ff41f6872dc4c
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2023-01-09 19:41:15 -0300

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express, { Express } from "express";
 import cors from "cors";
 import dotenv from 'dotenv'
+import { AddressInfo } from 'net'
+
 dotenv.config()
 
-const app: Express = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT || 3003, ()=>{
-    console.log(`Servidor rodando na porta ${process.env.PORT  || 3003}`)
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`)
+    } else {
+        console.error(`Failure upon starting server.`)
+    }
 })
 
 export default app;
